54d9e660033142589c4725da3db4bcc84bb19682
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-09-26 11:25:56 +0530

    Updated the notes in local Storage and fetch it , delete it [Project completed]

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener('click',()=>{
     addNewNote()
 })
@@ -29,6 +35,8 @@ function addNewNote(text = '') {
 
     deleteBtn.addEventListener('click',()=>{
         note.remove()
+
+        updateLS()
     })
 
     editBtn.addEventListener('click',()=>{
@@ -40,7 +48,19 @@ function addNewNote(text = '') {
         const  {value} = e.target
 
         main.innerHTML = marked(value)
+
+        updateLS()
     })
 
     document.body.appendChild(note)
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes',JSON.stringify(notes))
 }
